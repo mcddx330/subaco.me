4faedfeb3d8e5d9440f5d81b93dfeb1d595608e8
mcddx330 <monobeat.030+github@gmail.com>
2022-01-06 02:41:29 +0900

    ヘッダー、日記表示削除

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,3 @@ use Illuminate\Support\Facades\Route;
 Route::domain(env('APP_DOMAIN'))->group(function () {
     Route::get('/', ['as' => 'index', 'uses' => 'IndexController@index']);
 });
-
-Route::domain('diary.' . env('APP_DOMAIN'))->group(function () {
-    Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-        Route::get('/dashboard', function () {
-            return view('dashboard');
-        })->name('dashboard');
-    });
-
-    Route::get('/', [\App\Http\Controllers\DiaryController::class, 'index'])
-        ->name('diary.index');
-    Route::get('/{year}/{month}/{day}/{slug}', [\App\Http\Controllers\DiaryController::class, 'showArticle'])
-        ->name('diary.show_article');
-
-});
