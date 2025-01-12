ba7ae4d61c6dabc8b069caea0809faa4b9af76fb
mcddx330 <monobeat.030+github@gmail.com>
2025-01-12 23:36:24 +0900

    webpack -> vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-require("./fontawesome");
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/brands.css'; // fab
+import '@fortawesome/fontawesome-free/css/regular.css'; // far
+
+import "../css/app.css";
--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -12,9 +12,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{mix('css/style.css').'?'.time()}}">
-    <script src="{{mix('js/app.js').'?'.time()}}"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body id="view-{{ $body_id }}">
 @yield('header')
